bb06fb69f8cb698ee7e85cf5c90d1d0979e269d0
e540196 <minder@puzzle.ch>
2021-10-07 07:51:21 +0200

    Set windows default size

--- GUI.java
+++ GUI.java
@@ -16,9 +16,12 @@ public class GUI {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("GUI");
+        frame.setResizable(true);
         frame.setContentPane(new GUI().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+
+        frame.setSize(500, 500);
         frame.setVisible(true);
 
     }
